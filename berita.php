e0fa6616a2a056db6daaedd55e895846f5ee95a3
Ariel Gunawan <itsmeriell28@gmail.com>
2024-03-03 01:26:45 +0800

    dikit lagiiii

--- berita.php
+++ berita.php
@@ -34,74 +34,21 @@
               </form>
           </div>
         </nav>
-        <!-- Content -->
+        <!-- Content -->        
         <div class="container mt-5">
             <div class="row mb-2">
                 <div class="col-md-6">
                     <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
                         <div class="col p-4 d-flex flex-column position-static">
-                        <strong class="d-inline-block mb-2 text-success-emphasis">Design</strong>
-                        <h3 class="mb-0">Post title</h3>
-                        <div class="mb-1 text-body-secondary">Nov 11</div>
+                        <h3 class="mb-0">Nama Berita</h3>
+                        <div class="mb-1 text-body-secondary">Tanggal Berita</div>
                         <p class="mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
-                        <a href="#" class="icon-link text-white col-7 btn btn-primary gap-1 icon-link-hover stretched-link">
+                        <a href="detail-berita.html" class="text-white col-7 btn btn-primary gap-1 icon-link-hover stretched-link text-center">
                             Continue reading
-                            <svg class="bi"><use xlink:href="#chevron-right"/></svg>
                         </a>
                         </div>
                         <div class="col-auto d-none d-lg-block">
                         <img src="assets/img/tes (4).jpg" class="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" role="img">
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-6">
-                    <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
-                        <div class="col p-4 d-flex flex-column position-static">
-                        <strong class="d-inline-block mb-2 text-success-emphasis">Design</strong>
-                        <h3 class="mb-0">Post title</h3>
-                        <div class="mb-1 text-body-secondary">Nov 11</div>
-                        <p class="mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
-                        <a href="#" class="icon-link text-white col-7 btn btn-primary gap-1 icon-link-hover stretched-link">
-                            Continue reading
-                            <svg class="bi"><use xlink:href="#chevron-right"/></svg>
-                        </a>
-                        </div>
-                        <div class="col-auto d-none d-lg-block">
-                        <img src="assets/img/tes (3).jpg" class="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" role="img">
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-6">
-                    <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
-                        <div class="col p-4 d-flex flex-column position-static">
-                        <strong class="d-inline-block mb-2 text-success-emphasis">Design</strong>
-                        <h3 class="mb-0">Post title</h3>
-                        <div class="mb-1 text-body-secondary">Nov 11</div>
-                        <p class="mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
-                        <a href="#" class="icon-link text-white col-7 btn btn-primary gap-1 icon-link-hover stretched-link">
-                            Continue reading
-                            <svg class="bi"><use xlink:href="#chevron-right"/></svg>
-                        </a>
-                        </div>
-                        <div class="col-auto d-none d-lg-block">
-                        <img src="assets/img/tes (2).jpg" class="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" role="img">
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-6">
-                    <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
-                        <div class="col p-4 d-flex flex-column position-static">
-                        <strong class="d-inline-block mb-2 text-success-emphasis">Design</strong>
-                        <h3 class="mb-0">Post title</h3>
-                        <div class="mb-1 text-body-secondary">Nov 11</div>
-                        <p class="mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
-                        <a href="#" class="icon-link text-white col-7 btn btn-primary gap-1 icon-link-hover stretched-link">
-                            Continue reading
-                            <svg class="bi"><use xlink:href="#chevron-right"/></svg>
-                        </a>
-                        </div>
-                        <div class="col-auto d-none d-lg-block">
-                        <img src="assets/img/tes (1).jpg" class="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" role="img">
                         </div>
                     </div>
                 </div>
